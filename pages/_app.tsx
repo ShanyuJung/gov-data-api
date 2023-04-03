5fb1d5a460cb0aa1056804f3063474a37f7de02b
Luka <srps8912@gmail.com>
2023-04-03 21:30:49 +0800

    Feat: Add Header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header/Header";
 import type { AppProps } from "next/app";
 import { createGlobalStyle } from "styled-components";
 
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <Component {...pageProps} />
     </>
   );
