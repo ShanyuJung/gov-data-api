56e62cb17788e640924c4b51daf9f0c21b15933b
Luka <srps8912@gmail.com>
2023-04-03 21:12:45 +0800

    Style: Setting global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+* {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: 'Noto Sans TC', sans-serif;
+  }
+  
+  html,body {
+    min-width: 375px;
+}
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
